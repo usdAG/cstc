c463236cba1f29d1416edb8d0e4b611651dc4d2d
TNeitzel <qtc_de@outlook.com>
2020-03-19 09:01:15 +0100

    Imporve HttpSetUri operation
    The HttpSetUri operation had different flaws. It used superflous String
    transformations and did not allow to keep GET parameters. This was now
    improved and the function should work well also with raw byte input.

--- src/de/usd/cstchef/operations/setter/HttpSetUri.java
+++ src/de/usd/cstchef/operations/setter/HttpSetUri.java
@@ -1,45 +1,69 @@
 package de.usd.cstchef.operations.setter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 
+import javax.swing.JCheckBox;
+
+import burp.BurpUtils;
+import burp.IBurpExtenderCallbacks;
+import burp.IExtensionHelpers;
+import burp.IRequestInfo;
 import de.usd.cstchef.operations.Operation;
 import de.usd.cstchef.operations.OperationCategory;
 import de.usd.cstchef.operations.Operation.OperationInfos;
+import de.usd.cstchef.view.ui.FormatTextField;
 import de.usd.cstchef.view.ui.VariableTextField;
 
 @OperationInfos(name = "HTTP SET Uri", category = OperationCategory.SETTER, description = "Sets the given variable as the uri.")
 public class HttpSetUri extends Operation {
 
 	private VariableTextField uriTxt;
+	private JCheckBox checkbox;
 
 	@Override
 	public void createUI() {
 		this.uriTxt = new VariableTextField();
 		this.addUIElement("Uri", this.uriTxt);
+		
+		this.checkbox = new JCheckBox("Keep parameters");
+	    this.checkbox.setSelected(false);
+		this.addUIElement(null, this.checkbox);
 	}
 
+	
 	@Override
 	protected byte[] perform(byte[] input) throws Exception {
 		try {
-			// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-			String newUri = this.uriTxt.getText();
-			Reader in = new InputStreamReader(new ByteArrayInputStream(input));
-			BufferedReader reader = new BufferedReader(in);
-			String requestLine = reader.readLine();
-			String[] parts = requestLine.split(" ", 3);
-			StringBuffer buf = new StringBuffer();
-			String newRequestLine = String.format("%s %s %s", parts[0], newUri, parts[2]);
-			// TODO is there a better way to do this?
-			buf.append(newRequestLine).append("\n");
-			String line;
-			while ((line = reader.readLine()) != null) {
-				buf.append(line).append("\n");
+			IBurpExtenderCallbacks callbacks = BurpUtils.getInstance().getCallbacks();
+			IExtensionHelpers helpers = callbacks.getHelpers();
+			int length = input.length;
+			
+			int firstMark = helpers.indexOf(input, " ".getBytes(), false, 0, length);
+			int secondMark;
+			
+			if( this.checkbox.isSelected() ) {
+				secondMark = helpers.indexOf(input, "?".getBytes(), false, firstMark + 1, length);
+			} else {
+				secondMark = helpers.indexOf(input, " ".getBytes(), false, firstMark + 1, length);
 			}
 			
-			return buf.toString().getBytes();
+			byte[] method = Arrays.copyOfRange(input, 0, firstMark + 1);
+			byte[] newUri = this.uriTxt.getBytes();
+			byte[] rest = Arrays.copyOfRange(input, secondMark, length);
+			
+			byte[] newRequest = new byte[method.length + newUri.length + rest.length];
+			System.arraycopy(method, 0, newRequest, 0, method.length);
+			System.arraycopy(newUri, 0, newRequest, method.length, newUri.length);
+			System.arraycopy(rest, 0, newRequest, method.length + newUri.length, rest.length);
+			
+			return newRequest;
+			
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Provided input is not a valid http request.");
 		}
