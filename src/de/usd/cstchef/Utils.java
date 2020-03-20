6af0a503c82578c4767d8a816341931bb91816ed
TNeitzel <qtc_de@outlook.com>
2020-03-20 06:25:36 +0100

    Add missing import
    Inside the Utils class an import (Arrays) was missing.

--- src/de/usd/cstchef/Utils.java
+++ src/de/usd/cstchef/Utils.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
