a2a1dec20152c9f5cd25756e20d6967a90858b7b
TNeitzel <qtc_de@outlook.com>
2020-03-22 00:06:06 +0100

    Remove superflous string op in UriExtractor
    The HttpUriExtractor transformed its input into Strings before
    extraction. This was now changed to operate on the byte level.
    Furthermore, a checkbox was added to allow extracting Urls without
    parameters.

--- src/de/usd/cstchef/operations/extractors/HttpUriExtractor.java
+++ src/de/usd/cstchef/operations/extractors/HttpUriExtractor.java
@@ -4,23 +4,50 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Arrays;
 
+import javax.swing.JCheckBox;
+
+import burp.BurpUtils;
+import burp.IBurpExtenderCallbacks;
+import burp.IExtensionHelpers;
 import de.usd.cstchef.operations.Operation;
 import de.usd.cstchef.operations.OperationCategory;
 import de.usd.cstchef.operations.Operation.OperationInfos;
+import de.usd.cstchef.view.ui.VariableTextField;
 
 @OperationInfos(name = "HTTP URI", category = OperationCategory.EXTRACTORS, description = "Extracts the URI of a HTTP request.")
 public class HttpUriExtractor extends Operation {
 
+	private JCheckBox checkbox;
+
+	@Override
+	public void createUI() {
+		this.checkbox = new JCheckBox("With parameters");
+	    this.checkbox.setSelected(true);
+		this.addUIElement(null, this.checkbox);
+	}
+	
 	@Override
 	protected byte[] perform(byte[] input) throws Exception {
 		try {
-			// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-			Reader in = new InputStreamReader(new ByteArrayInputStream(input));
-			BufferedReader reader = new BufferedReader(in);
-			String requestLine = reader.readLine();
-			String[] parts = requestLine.split(" ");
-			return parts[1].getBytes();
+			
+			IBurpExtenderCallbacks callbacks = BurpUtils.getInstance().getCallbacks();
+			IExtensionHelpers helpers = callbacks.getHelpers();
+			int length = input.length;
+			
+			int firstMark = helpers.indexOf(input, " ".getBytes(), false, 0, length);
+			int lineMark = helpers.indexOf(input, " ".getBytes(), false, firstMark + 1, length);
+			
+			int secondMark = helpers.indexOf(input, "?".getBytes(), false, firstMark + 1, length);
+					
+			if( this.checkbox.isSelected() || secondMark < 0 || secondMark >= lineMark) {
+				secondMark = lineMark;
+			}
+			
+			byte[] result = Arrays.copyOfRange(input, firstMark + 1, secondMark);
+			return result;
+			
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Provided input is not a valid http request.");
 		}
