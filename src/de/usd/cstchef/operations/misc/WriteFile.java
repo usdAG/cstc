d565386995e48b34c6360aacfd8921feebeeb50a
TNeitzel <qtc_de@outlook.com>
2020-03-23 08:31:22 +0100

    Set filename field read only in writefile
    The WriteFile operation allowed users to enter a path manually. This
    would created a single file for each keystorke, until the path is
    complete. To prevent this, the input field was now set to readonly. This
    forces users to select a filename by using the JFileChooser.

--- src/de/usd/cstchef/operations/misc/WriteFile.java
+++ src/de/usd/cstchef/operations/misc/WriteFile.java
@@ -25,7 +25,6 @@ public class WriteFile extends Operation implements ActionListener {
 	@Override
 	protected byte[] perform(byte[] input) throws Exception {
 		String path = fileNameTxt.getText();
-		//TODO add ok button? if changed manually, 100 Files are created :D
 		
 		if (!lastPath.equals(path)) {
 			if (out != null) {
@@ -48,6 +47,7 @@ public class WriteFile extends Operation implements ActionListener {
 
 	public void createUI() {
 		this.fileNameTxt = new VariableTextField();
+		this.fileNameTxt.setEditable(false);
 		this.addUIElement("Filename", this.fileNameTxt);
 
 		JButton chooseFileButton = new JButton("Select file");
