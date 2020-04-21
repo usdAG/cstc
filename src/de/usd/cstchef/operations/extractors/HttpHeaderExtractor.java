a765b49bd34d3c8a5b8f6f68b531fa62ea07eeec
TNeitzel <qtc_de@outlook.com>
2020-04-21 21:10:13 +0200

    Improve HttpHeaderExtractor
    Improved the HttpHeaderExtractor. It contained a superflous string
    transformation which was removed.

--- src/de/usd/cstchef/operations/extractors/HttpHeaderExtractor.java
+++ src/de/usd/cstchef/operations/extractors/HttpHeaderExtractor.java
@@ -10,58 +10,47 @@ import de.usd.cstchef.operations.Operation.OperationInfos;
 import de.usd.cstchef.operations.OperationCategory;
 import de.usd.cstchef.view.ui.VariableTextField;
 
-@OperationInfos(name = "HTTP Header", category = OperationCategory.EXTRACTORS, description = "Extracts a header of a HTTP request.")
+@OperationInfos(name = "HTTP header", category = OperationCategory.EXTRACTORS, description = "Extracts a header of a HTTP request.")
 public class HttpHeaderExtractor extends Operation {
 
-	private VariableTextField headerTxt;
+	private VariableTextField headerNameField;
 
 	@Override
 	protected byte[] perform(byte[] input) throws Exception {
 		
-		try {
-			
-			String headerName = "\r\n" + headerTxt.getText() + ":";
-			IBurpExtenderCallbacks callbacks = BurpUtils.getInstance().getCallbacks();
-			IExtensionHelpers helpers = callbacks.getHelpers();
-			
-			int nameStart = helpers.indexOf(input, headerName.getBytes(), true, 0, input.length);
-			
-			if( nameStart < 0 )
-				throw new HeaderNotFoundException("Header not found.");
-			
-			int valueStart = helpers.indexOf(input, " ".getBytes(), false, nameStart, input.length);
-			int valueEnd = helpers.indexOf(input, "\r\n".getBytes(), false, valueStart, input.length);
-			
-			if( valueStart < 0 || valueEnd < 0)
-				throw new HeaderInvalidFormat("Invalid Header format.");
-			
-			byte[] result = Arrays.copyOfRange(input, valueStart + 1, valueEnd);
-			return result;
-			
-		} catch(HeaderInvalidFormat e) {
-			throw new IllegalArgumentException("Invalid Header format.");
-		} catch( HeaderNotFoundException e ) {
+		byte[] headerName = headerNameField.getBytes();
+		if( headerName.length == 0 )
+			return input;
+		
+		byte[] headerSearch = new byte[headerName.length + 4];
+		System.arraycopy("\r\n".getBytes(), 0, headerSearch, 0, 2);
+		System.arraycopy(headerName, 0, headerSearch, 2, headerName.length);
+		System.arraycopy(": ".getBytes(), 0, headerSearch, headerName.length + 2, 2);
+		
+		IBurpExtenderCallbacks callbacks = BurpUtils.getInstance().getCallbacks();
+		IExtensionHelpers helpers = callbacks.getHelpers();
+		int length = input.length;
+		
+		int offset = helpers.indexOf(input, headerSearch, true, 0, length);
+		
+		if( offset < 0 )
 			throw new IllegalArgumentException("Header not found.");
-		} catch (Exception e) {
-			throw new IllegalArgumentException("Provided input is not a valid http request.");
-		}
+		
+		int valueStart = helpers.indexOf(input, " ".getBytes(), false, offset, length);
+		if( valueStart < 0 )
+			throw new IllegalArgumentException("Invalid Header format.");
+		int valueEnd = helpers.indexOf(input, "\r\n".getBytes(), false, valueStart, length);
+		if( valueEnd < 0 )
+			throw new IllegalArgumentException("Invalid Header format.");
+		
+		byte[] result = Arrays.copyOfRange(input, valueStart + 1, valueEnd);
+		return result;
 	}
 
 	@Override
 	public void createUI() {
-		this.headerTxt = new VariableTextField();
-		this.addUIElement("Name", this.headerTxt);
+		this.headerNameField = new VariableTextField();
+		this.addUIElement("Name", this.headerNameField);
 	}
 	
-	class HeaderNotFoundException extends Exception {
-	      public HeaderNotFoundException(String message) {
-	         super(message);
-	      }
-	 }
-	
-	class HeaderInvalidFormat extends Exception {
-	      public HeaderInvalidFormat(String message) {
-	         super(message);
-	      }
-	 }
 }
