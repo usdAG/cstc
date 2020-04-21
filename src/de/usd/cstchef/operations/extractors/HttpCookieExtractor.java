5ebca12db573264c1196ec53e2a76b93860515fe
TNeitzel <qtc_de@outlook.com>
2020-04-21 21:31:34 +0200

    Improve HttpCookieExtractor
    Improved the search for the Cookie header inside the
    HttpCookieExtractor. This should make false positives more unlikely.

--- src/de/usd/cstchef/operations/extractors/HttpCookieExtractor.java
+++ src/de/usd/cstchef/operations/extractors/HttpCookieExtractor.java
@@ -18,38 +18,38 @@ public class HttpCookieExtractor extends Operation {
 
 	@Override
 	protected byte[] perform(byte[] input) throws Exception {
-
+		
 		byte[] cookieName = cookieNameField.getBytes();
 		if( cookieName.length == 0 )
 			return input;
-
+		
 		byte[] cookieSearch = new byte[cookieName.length + 1];
 		System.arraycopy(cookieName, 0, cookieSearch, 0, cookieName.length);
 		System.arraycopy("=".getBytes(), 0, cookieSearch, cookieName.length, 1);
-
+		
 		IBurpExtenderCallbacks callbacks = BurpUtils.getInstance().getCallbacks();
 		IExtensionHelpers helpers = callbacks.getHelpers();
 		int length = input.length;
-
+		
 		IResponseInfo resp = helpers.analyzeResponse(input);
 		boolean isRequest = (resp.getStatusCode() == 0);
-
-		String cookieHeader = "Set-Cookie: ";
+		
+		String cookieHeader = "\r\nSet-Cookie: ";
 		if(isRequest)
-			cookieHeader = "Cookie: ";
-
+			cookieHeader = "\r\nCookie: ";
+		
 		try {
-
+			
 			int offset = helpers.indexOf(input, cookieHeader.getBytes(), false, 0, length);
-			int line_end = helpers.indexOf(input, "\r\n".getBytes(), false, offset, length);
+			int line_end = helpers.indexOf(input, "\r\n".getBytes(), false, offset + 2, length);
 			int start = helpers.indexOf(input, cookieSearch, true, offset, line_end);
 			int end = helpers.indexOf(input, ";".getBytes(), true, start, line_end);
-
+			
 			if( end < 0 )
 				end = line_end;
-
+			
 			return Arrays.copyOfRange(input, start + cookieName.length + 1, end);
-
+			
 		} catch( IllegalArgumentException e ) {
 			throw new IllegalArgumentException("Cookie not found.");
 		}
