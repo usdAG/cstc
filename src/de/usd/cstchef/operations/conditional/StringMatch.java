9b3a8e5d6827792468f151566348a0d0d83ffb52
TNeitzel <qtc_de@outlook.com>
2020-06-23 20:39:53 +0200

    Add StringMatch and StringContains conditional op
    Two simple conditional operations for string matching / contains.

---
+++ src/de/usd/cstchef/operations/conditional/StringMatch.java
@@ -0,0 +1,56 @@
+package de.usd.cstchef.operations.conditional;
+
+import javax.swing.JCheckBox;
+
+import burp.BurpUtils;
+import burp.IBurpExtenderCallbacks;
+import burp.IExtensionHelpers;
+import de.usd.cstchef.operations.Operation.OperationInfos;
+import de.usd.cstchef.operations.OperationCategory;
+
+@OperationInfos(name = "String Match", category = OperationCategory.CONDITIONAL, description = "Skip if input matches")
+public class StringMatch extends Conditionaloperation {
+
+	private JCheckBox invert;
+	private JCheckBox caseSensitive;
+
+	@Override
+	protected byte[] perform(byte[] input) throws Exception {
+		
+		byte[] search = this.expr.getBytes();
+		if( search.length != input.length ) {
+			if( invert.isSelected() ) {
+				this.setOperationSkip();
+				this.setLaneSkip();
+			} else {
+				this.resetSkips();
+			}
+			return input;
+		}
+		
+		IBurpExtenderCallbacks cbs = BurpUtils.getInstance().getCallbacks();
+		IExtensionHelpers helpers = cbs.getHelpers();
+		int start = helpers.indexOf(input, search, caseSensitive.isSelected(), 0, input.length);
+		
+		if( (start >= 0) ^ invert.isSelected() ) {
+			this.setOperationSkip();
+			this.setLaneSkip();
+		} else {
+			this.resetSkips();
+		}
+		
+		return input;
+	}
+	
+	@Override
+	public void createUI() {
+		super.createUI();
+		
+		this.invert = new JCheckBox();
+		this.addUIElement("Invert Match", this.invert);
+		
+		this.caseSensitive = new JCheckBox();
+		this.addUIElement("Case Sensitive", this.caseSensitive);
+	}
+	
+}
