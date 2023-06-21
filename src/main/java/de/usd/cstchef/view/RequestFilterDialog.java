4509f2e96349cc4508a2cbe91da34fcfaaf2d601
Niklas Herz <nklherz@gmail.com>
2023-06-21 16:14:35 +0200

    change layout of the filter window to non-tabbed

--- src/main/java/de/usd/cstchef/view/RequestFilterDialog.java
+++ src/main/java/de/usd/cstchef/view/RequestFilterDialog.java
@@ -4,7 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -14,31 +17,39 @@ import javax.swing.JTabbedPane;
 import org.objectweb.asm.Label;
 
 import burp.IBurpExtenderCallbacks;
+import de.usd.cstchef.FilterState;
 import de.usd.cstchef.FilterState.BurpOperation;
 
-public class RequestFilterDialog extends JTabbedPane {
+public class RequestFilterDialog extends JPanel {
     private LinkedHashMap<Filter, Boolean> incomingFilterSettings;
     private LinkedHashMap<Filter, Boolean> outgoingFilterSettings;
     private LinkedHashMap<Filter, Boolean> formatFilterSettings;
 
     public RequestFilterDialog() {
+        this.setLayout(new GridLayout(0, 4));
+
         JPanel incomingPanel = createPanel(BurpOperation.INCOMING);
         JPanel outgoingPanel = createPanel(BurpOperation.OUTGOING);
         JPanel formatPanel = createPanel(BurpOperation.FORMAT);
 
-        // JTabbedPane tabPane = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new GridLayout(7, 0));
+        labelPanel.add(new JLabel(""));
+        List<String> labels = Arrays.asList("Proxy","Repeater","Spider","Scanner","Intruder","Extender");
+        for(String label : labels){
+            labelPanel.add(new JLabel(label));
+        }
 
-        this.addTab("Incoming", incomingPanel);
-        this.addTab("Outgoing", outgoingPanel);
-        this.addTab("Format", formatPanel);
+        this.add(labelPanel);
+        this.add("Incoming", incomingPanel);
+        this.add("Outgoing", outgoingPanel);
+        this.add("Format", formatPanel);
 
-        // this.add(tabPane, BorderLayout.CENTER);
-        
     }
 
     private JPanel createPanel(BurpOperation operation) {
         LinkedHashMap<Filter, Boolean> filterSettings;
-
+        
         filterSettings = new LinkedHashMap<>();
         filterSettings.put(new Filter("Proxy", IBurpExtenderCallbacks.TOOL_PROXY), false);
         filterSettings.put(new Filter("Repeater", IBurpExtenderCallbacks.TOOL_REPEATER), false);
@@ -48,10 +59,10 @@ public class RequestFilterDialog extends JTabbedPane {
         filterSettings.put(new Filter("Extender", IBurpExtenderCallbacks.TOOL_EXTENDER), false);
 
         JPanel panel = new JPanel();
+        panel.add(new JLabel(FilterState.translateBurpOperation(operation)));
         for (Map.Entry<Filter, Boolean> entry : filterSettings.entrySet()) {
             Filter filter = entry.getKey();
             boolean selected = entry.getValue();
-            panel.add(new JLabel(filter.getName() + ": "));
 
             JCheckBox box = new JCheckBox();
             box.setSelected(selected);
@@ -73,7 +84,7 @@ public class RequestFilterDialog extends JTabbedPane {
             case FORMAT:
                 formatFilterSettings = filterSettings;
         }
-        panel.setLayout(new GridLayout(0, 2));
+        panel.setLayout(new GridLayout(7, 0));
         return panel;
     }
 
