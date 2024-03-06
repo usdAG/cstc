cbebf5be528786537259f68dce43f33dc01333ac
Niklas Herz <nklherz@gmail.com>
2024-03-06 14:39:39 +0100

    add xml extractor for responses

--- src/main/java/de/usd/cstchef/operations/extractors/HttpXmlExtractor.java
+++ src/main/java/de/usd/cstchef/operations/extractors/HttpXmlExtractor.java
@@ -1,21 +1,27 @@
 package de.usd.cstchef.operations.extractors;
 
-import java.util.Arrays;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
 
 import javax.swing.JTextField;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-import burp.BurpUtils;
-import burp.api.montoya.MontoyaApi;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 import burp.api.montoya.core.ByteArray;
 import burp.api.montoya.http.message.params.HttpParameterType;
-import burp.api.montoya.http.message.params.ParsedHttpParameter;
 import burp.api.montoya.http.message.requests.HttpRequest;
+import burp.api.montoya.http.message.responses.HttpResponse;
 import de.usd.cstchef.Utils.MessageType;
 import de.usd.cstchef.operations.Operation;
 import de.usd.cstchef.operations.Operation.OperationInfos;
 import de.usd.cstchef.operations.OperationCategory;
 
-@OperationInfos(name = "HTTP XML", category = OperationCategory.EXTRACTORS, description = "Extract XML value from HTTP message.")
+@OperationInfos(name = "HTTP XML", category = OperationCategory.EXTRACTORS, description = "Extract the first occurrence of a XML value from HTTP message.")
 public class HttpXmlExtractor extends Operation {
 
     private JTextField fieldTxt;
@@ -27,11 +33,21 @@ public class HttpXmlExtractor extends Operation {
         if (keyName.equals(""))
             return ByteArray.byteArray(0);
 
-        try{
-            return ByteArray.byteArray(HttpRequest.httpRequest(input).parameterValue(keyName, HttpParameterType.XML));
-        }
-        catch(Exception e){
-            throw new IllegalArgumentException("Input is not a valid request");
+        if (messageType == MessageType.REQUEST) {
+            try {
+                return ByteArray
+                        .byteArray(HttpRequest.httpRequest(input).parameterValue(keyName, HttpParameterType.XML));
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Input is not a valid request");
+            }
+        } else if (messageType == MessageType.RESPONSE) {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(new ByteArrayInputStream(HttpResponse.httpResponse(input).bodyToString().getBytes()));
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName(keyName);
+            return ByteArray.byteArray(nodeList.item(0).getTextContent());
+        } else {
+            return parseRawMessage(input);
         }
     }
 
