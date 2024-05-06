c2ff1925928801f0bf19efe2e69adbf65ffa4e67
Felix Buschbeck <felixb15@proton.me>
2024-05-06 12:23:12 -0400

    add methods

--- src/test/java/de/usd/cstchef/utils/UnitTestObjectFactory.java
+++ src/test/java/de/usd/cstchef/utils/UnitTestObjectFactory.java
@@ -2,7 +2,11 @@ package de.usd.cstchef.utils;
 
 import burp.CstcObjectFactory;
 import burp.api.montoya.core.ByteArray;
+import burp.api.montoya.http.message.requests.HttpRequest;
+import burp.api.montoya.http.message.responses.HttpResponse;
 import burp.objects.CstcByteArray;
+import burp.objects.CstcHttpRequest;
+import burp.objects.CstcHttpResponse;
 
 public class UnitTestObjectFactory implements CstcObjectFactory{
 
@@ -19,6 +23,26 @@ public class UnitTestObjectFactory implements CstcObjectFactory{
     @Override
     public ByteArray createByteArray(int i) {
         return CstcByteArray.byteArray(i);
+    }
+
+    @Override
+    public ByteArray getHttpRequestBody(ByteArray request) {
+        return new CstcHttpRequest(request).body();
+    }
+
+    @Override
+    public ByteArray getHttpResponseBody(ByteArray response) {
+        return new CstcHttpResponse(response).body();
+    }
+
+    @Override
+    public HttpRequest createHttpRequest(ByteArray request) {
+        return new CstcHttpRequest(request);
+    }
+
+    @Override
+    public HttpResponse createHttpResponse(ByteArray response) {
+        return new CstcHttpResponse(response);
     }
     
 }
