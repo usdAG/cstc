71083e507df5fe9b9bff76607f3b5d8c951edd1c
Felix Buschbeck <felixb15@proton.me>
2024-10-07 05:19:18 -0400

    Refactor operation logic

--- src/main/java/de/usd/cstchef/operations/setter/HttpXmlSetter.java
+++ src/main/java/de/usd/cstchef/operations/setter/HttpXmlSetter.java
@@ -1,28 +1,11 @@
 package de.usd.cstchef.operations.setter;
 
-import java.io.ByteArrayInputStream;
-import java.io.StringWriter;
-
 import javax.swing.JCheckBox;
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 import burp.api.montoya.core.ByteArray;
-import burp.api.montoya.http.message.HttpMessage;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.http.message.responses.HttpResponse;
+import de.usd.cstchef.Utils;
 import de.usd.cstchef.Utils.MessageType;
 import de.usd.cstchef.operations.Operation;
 import de.usd.cstchef.operations.Operation.OperationInfos;
@@ -46,64 +29,11 @@ public class HttpXmlSetter extends Operation {
             return input;
         }
 
-        if (messageType == MessageType.REQUEST || messageType == MessageType.RESPONSE) {
-
-            HttpMessage httpMessage;
-            httpMessage = messageType == MessageType.REQUEST ? HttpRequest.httpRequest(input) : HttpResponse.httpResponse(input);
-
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(true);
-            dbf.setXIncludeAware(false);
-            // XXE
-            dbf.setExpandEntityReferences(false);
-            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
-            Document doc = messageType == MessageType.REQUEST ? dbf.newDocumentBuilder().parse(new ByteArrayInputStream(factory.createHttpRequest(input).bodyToString().getBytes())) : 
-                dbf.newDocumentBuilder().parse(new ByteArrayInputStream(factory.createHttpResponse(input).bodyToString().getBytes()));
-            doc.getDocumentElement().normalize();
-
-            Element toAdd;
-
-            XPath xPath = XPathFactory.newInstance().newXPath();
-            NodeList nodeList;
-
-            try {
-                nodeList = (NodeList) xPath.compile(p).evaluate(doc, XPathConstants.NODESET);
-            }
-            catch(Exception e) {
-                throw new IllegalArgumentException("Invalid XPath Syntax.");
-            }
-
-            for(int i = 0; i < nodeList.getLength(); i++) {
-                nodeList.item(i).setTextContent(v);
-            }
-
-            if(nodeList.getLength() == 0 && addIfNotPresent.isSelected()) {
-                if(p.matches(".*/@[a-zA-Z0-9-_.]*")) {
-                    nodeList = (NodeList) xPath.compile(p.replaceAll("/@[a-zA-Z0-9-_.]*$", "")).evaluate(doc, XPathConstants.NODESET);
-                    for(int i = 0; i < nodeList.getLength(); i++) {
-                        ((Element) nodeList.item(i)).setAttribute(p.split("@")[p.split("@").length - 1], v);
-                    }
-                }
-                else {
-                    nodeList = (NodeList) xPath.compile(p.replaceAll("/[a-zA-Z0-9-_.]*$", "")).evaluate(doc, XPathConstants.NODESET);
-                    for(int i = 0; i < nodeList.getLength(); i++) {
-                        toAdd = doc.createElement(p.split("/")[p.split("/").length - 1]);
-                        toAdd.setTextContent(v);
-                        nodeList.item(i).appendChild(toAdd);
-                    }
-                }
-            }
-
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            transformerFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
-            Transformer xformer = transformerFactory.newTransformer();
-            xformer.setOutputProperty(OutputKeys.INDENT, "no");
-
-            StringWriter output = new StringWriter();
-            xformer.transform(new DOMSource(doc), new StreamResult(output));
-            return messageType == MessageType.REQUEST ? ((HttpRequest)httpMessage).withBody(output.toString()).toByteArray() : ((HttpResponse)httpMessage).withBody(output.toString()).toByteArray();
+        if(messageType == MessageType.REQUEST || messageType == MessageType.RESPONSE) {
+            return HttpRequest.httpRequest(input).withBody(Utils.xmlSetter(HttpRequest.httpRequest(input).body(), p, v, addIfNotPresent.isSelected())).toByteArray();
+        }
+        else if(messageType == MessageType.RESPONSE) {
+            return HttpResponse.httpResponse(input).withBody(Utils.xmlSetter(HttpResponse.httpResponse(input).body(), p, v, addIfNotPresent.isSelected())).toByteArray();
         }
         else {
             return parseRawMessage(input);
