6b54686f1b04b5693a9e47afd721bc710e90952f
Felix Buschbeck <felixb15@proton.me>
2024-11-26 18:03:26 +0100

    Change naming of padding mechanism ComboBox option

--- src/main/java/de/usd/cstchef/operations/encryption/EncryptionOperation.java
+++ src/main/java/de/usd/cstchef/operations/encryption/EncryptionOperation.java
@@ -13,8 +13,20 @@ public abstract class EncryptionOperation extends CryptOperation {
 
     @Override
     protected ByteArray perform(ByteArray input, MessageType messageType) throws Exception {
+
+        /*
+         * javax.crypto.Cipher uses PKCS7 internally but names it PKCS5.
+         * The only difference between the two padding mechanisms is the
+         * block size they're working with and to not confuse the user
+         * we name the combobox item accordingly (see GitHub Issue #166)
+         */
+        String padding = (String) this.paddings.getSelectedItem();
+        if(padding.equals("PKCS5PADDING / PKCS7PADDING")) {
+            padding = "PKCS5PADDING";
+        }
+
         return factory.createByteArray(this.crypt(input.getBytes(), Cipher.ENCRYPT_MODE, this.algorithm, (String) this.cipherMode.getSelectedItem(),
-                (String) this.paddings.getSelectedItem()));
+                padding));
     }
 
 }
