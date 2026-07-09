e7e075b1478c007284c1b6f22a69c5ac65762e14
kaitoz11 <43519768+kaitoz11@users.noreply.github.com>
2026-07-09 14:57:48 +0700

    feat: add EdDSA sigining module

--- src/main/java/de/usd/cstchef/Utils.java
+++ src/main/java/de/usd/cstchef/Utils.java
@@ -137,6 +137,7 @@ import de.usd.cstchef.operations.setter.HttpXmlSetter;
 import de.usd.cstchef.operations.setter.XmlSetter;
 import de.usd.cstchef.operations.setter.JsonSetter;
 import de.usd.cstchef.operations.setter.LineSetter;
+import de.usd.cstchef.operations.signature.EdDsaSignature;
 import de.usd.cstchef.operations.signature.JWTDecode;
 import de.usd.cstchef.operations.signature.JWTSign;
 import de.usd.cstchef.operations.signature.RsaSignature;
@@ -389,6 +390,7 @@ public class Utils {
                 Counter.class, Concatenate.class,
                 DateTime.class, Deflate.class, DesDecryption.class, DesEncryption.class,
                 Divide.class, DivideList.class, DSTU7564.class,
+                EdDsaSignature.class,
                 FromBase64.class, FromDecimal.class, FromHex.class,
                 GetVariable.class, Gost.class, GUnzip.class, Gzip.class,
                 Hmac.class, HttpBodyExtractor.class, HttpCookieExtractor.class, HttpGetExtractor.class,
