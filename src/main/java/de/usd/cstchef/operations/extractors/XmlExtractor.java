6522086c8aa868e3e0c0497b6085cb36ea418600
Felix Buschbeck <felixb15@proton.me>
2026-07-29 12:31:20 +0200

    Add new XmlExtractor operation

---
+++ src/main/java/de/usd/cstchef/operations/extractors/XmlExtractor.java
@@ -0,0 +1,73 @@
+package de.usd.cstchef.operations.extractors;
+
+import burp.api.montoya.core.ByteArray;
+import de.usd.cstchef.Utils;
+import de.usd.cstchef.operations.Operation;
+import de.usd.cstchef.operations.Operation.OperationInfos;
+import de.usd.cstchef.view.ui.VariableTextField;
+import de.usd.cstchef.operations.OperationCategory;
+
+@OperationInfos(
+    name = "Get XML",
+    category = OperationCategory.EXTRACTORS,
+    description = """
+            <html>
+              <body>
+                <p>
+                  This operation uses XPath expressions. More information: https://www.w3.org/TR/xpath/
+                </p>
+
+                <h2>Selecting Nodes</h2>
+                <table cellspacing="0" cellpadding="3">
+                  <tr><td><code>nodename</code></td><td>Selects all nodes with the name <code>nodename</code>.</td></tr>
+                  <tr><td><code>/</code></td><td>Selects from the root node.</td></tr>
+                  <tr><td><code>//</code></td><td>Selects nodes in the document from the current node that match the selection no matter where they are.</td></tr>
+                  <tr><td><code>.</code></td><td>Selects the current node.</td></tr>
+                  <tr><td><code>..</code></td><td>Selects the parent of the current node.</td></tr>
+                  <tr><td><code>@</code></td><td>Selects attributes.</td></tr>
+                </table>
+
+                <h2>Predicates</h2>
+                <table cellspacing="0" cellpadding="3">
+                  <tr><td><code>/parent/child[1]</code></td><td>Selects the first <code>child</code> element that is the child of the <code>parent</code> element.</td></tr>
+                  <tr><td><code>/parent/child[last()]</code></td><td>Selects the last <code>child</code> element that is the child of the <code>parent</code> element.</td></tr>
+                  <tr><td><code>/parent/child[last()-1]</code></td><td>Selects the last but one <code>child</code> element that is the child of the <code>parent</code> element.</td></tr>
+                  <tr><td><code>/parent/child[position()&lt;3]</code></td><td>Selects the first two <code>child</code> elements that are children of the <code>parent</code> element.</td></tr>
+                  <tr><td><code>//child[@attr]</code></td><td>Selects all the <code>child</code> elements that have an attribute named <code>attr</code>.</td></tr>
+                  <tr><td><code>//child[@attr='xy']</code></td><td>Selects all the <code>child</code> elements that have an <code>attr</code> attribute with a value of <code>xy</code>.</td></tr>
+                  <tr><td><code>/parent/child[value&gt;35.00]</code></td><td>Selects all the <code>child</code> elements of the <code>parent</code> element that have a <code>value</code> element with a value greater than <code>35.00</code>.</td></tr>
+                  <tr><td><code>/parent/child[value&gt;35.00]/leaf</code></td><td>Selects all the <code>leaf</code> elements of the <code>child</code> elements of the <code>parent</code> element that have a <code>value</code> element with a value greater than <code>35.00</code>.</td></tr>
+                </table>
+
+                <h2>Selecting Unknown Nodes</h2>
+                <table cellspacing="0" cellpadding="3">
+                  <tr><td><code>*</code></td><td>Matches any element node.</td></tr>
+                  <tr><td><code>@*</code></td><td>Matches any attribute node.</td></tr>
+                  <tr><td><code>node()</code></td><td>Matches any node of any kind.</td></tr>
+                </table>
+              </body>
+            </html>
+            """
+)
+public class XmlExtractor extends Operation {
+
+    protected VariableTextField path;
+
+    @Override
+    protected ByteArray perform(ByteArray input) throws Exception {
+
+        String p = this.path.getText();
+
+        if(p.trim().isEmpty()) {
+            return input;
+        }
+
+        return Utils.xmlExtractor(input, p);
+    }
+
+    @Override
+    public void createUI() {
+        this.path = new VariableTextField();
+        this.addUIElement("Path", this.path);
+    }
+}
